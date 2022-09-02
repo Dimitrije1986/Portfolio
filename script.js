59db0e851a51031050a5fc84e97940576b745117
Dimitrije1986 <dimitrijekrnic@gmail.com>
2022-09-02 10:02:30 +0200

    animation added

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ btn.addEventListener('click', e => {
 	}
     
  });
+
+// animation
+
+let aboutMeBox = document.querySelector('.about-me');
+let skillsBox = document.querySelector('.skills');
+
+let sectionForAnimation = document.getElementById('section2');
+let sectionPosition = sectionForAnimation.getBoundingClientRect().top;
+let screenPosition = window.innerHeight;
+
+let project = document.querySelector('.project-wrapper');
+
+
+window.onscroll = () => {
+	
+	if (sectionPosition = screenPosition) {
+			aboutMeBox.classList.remove("animated-a");
+			skillsBox.classList.remove("animated-s");
+			project.classList.remove("animated-p")
+	}
+	
+}
+
