3f5c668c561ef8ab49daf12db33bd766e7e0fe21
Dimitrije1986 <dimitrijekrnic@gmail.com>
2022-11-05 16:27:53 +0100

    new changes

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-let list = document.querySelector('.header ul');
-let btn = document.querySelector('.header button');
-let img = document.querySelector('.header button img');
+let list = document.querySelector(".header ul");
+let btn = document.querySelector(".header button");
+let img = document.querySelector(".header button img");
 
-
-btn.addEventListener('click', e => {
-
-	if(img.alt === "menu"){
-		list.style.display = 'block';
-		img.alt = "close";
-	}else {
-		list.style.display = 'none';
-		img.alt = "menu";
-	}
-    
- });
-
-
-// animation
-
-/*let aboutMeBox = document.querySelector('.about-me');
-let skillsBox = document.querySelector('.skills');
-
-let sectionForAnimation = document.querySelector('#section2');
-let sectionPosition = sectionForAnimation.getBoundingClientRect().top;
-let screenPosition = window.innerHeight / 1.3;
-
-
-
-window.onscroll = () => {
-	if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400){ 
-		  aboutMeBox.classList.remove('slideFromLeft');
-		  skillsBox.classList.remove('slideFromRight');
-   }
-}*/
-
-
-
-
-
+btn.addEventListener("click", (e) => {
+  if (img.alt === "menu") {
+    list.style.display = "block";
+    img.alt = "close";
+  } else {
+    list.style.display = "none";
+    img.alt = "menu";
+  }
+});
